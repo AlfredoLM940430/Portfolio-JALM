c9250fbd0d7938173df4e7db90d4a52ee9ca907d
Alfredo <alfredo.lm@392.gmail.com>
2023-10-27 00:15:07 -0600

    footer

--- src/views/FooterView.jsx
+++ src/views/FooterView.jsx
@@ -13,14 +13,14 @@ export const FooterView = () => {
                     <a className="btn btn-c" href="José Alfredo López Mares.pdf">CV</a>
                 </nav>
                 <div className="d-flex justify-content-center">
-                    <a className="nombre" href="#header">
+                    <a className="nombre row" href="#header">
                         <picture>
                             <source srcSet="/img/al-logo-bg.avif" type="image/avif"/>
                             <source srcSet="/img/al-logo-bg.webp" type="image/webp"/>
                             <img src="/img/al-logo-bg.png" alt="Logo AL" width="30px"/>
                         </picture>  
+                        <p>{fecha.getFullYear()}</p>
                     </a>
-                    <p>{fecha.getFullYear()}</p>
                 </div>
             </footer>
         </>
